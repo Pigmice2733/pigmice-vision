3a035602c63dd61724ce6b4d5e6a22659290f5f0
Howard Abrams <howard.abrams@gmail.com>
2018-01-28 10:24:22 -0800

    Add unit tests for the smoothing function
    While we don't really need to test the various smoothing functions, we should at
    least verify that our code works by simply calling the smooth function in a test
    environment.

--- tests/test_graph.py
+++ tests/test_graph.py
@@ -6,20 +6,46 @@ to demonstrate visually how the smooth function can work.
 """
 
 from context import lib  # flake8: noqa
-from lib import util
+from lib import graph
 import numpy as np
 from numpy.random import randn
 import matplotlib.pyplot as plt
 
 def test_smooth_hanning():
-    "Assume we "
+    """
+    Take a series of numbers along a slope, and verify that the hanning function
+    returns expected values. This really tests that the system works correctly more
+    than testing the Hanning algorithm itself.
+    """
+    data = np.array([-0.6, 0.0, -0.6, -1.0, -0.4, 0.1, 0.6, 1.0, 0.4, 0.0, 0.4])
+    expected = np.array([0.0, -0.6, 0.0, -0.6, -1.0, -0.4, 0.1, 0.6, 1.0, 0.4, 0.0, 0.4, 0.0])
+    y = graph.smooth(data, 3, 'hanning')
+    assert np.array_equiv(y, expected)
+
+
+def test_slope_to_curve():
+    """
+    Keep in mind the smoothing function assumes some sort of sine-line wave, so
+    a simple slope won't work as expected. This test is really a demonstration
+    of its behavior, but validates that the system is working correctly.
+    """
+    data = np.array([0.0, 1.0, 2.0, 3.0, 4.0, 5.0, 6.0])
+    expected=np.array([1.0, 0.0, 1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 5.0])
+    y = graph.smooth(data, 3)
+    assert np.array_equal(y, expected)
 
 
 def smooth_demo():
+    """
+    This demonstration creates a series of numbers along a sine curve, and then
+    slightly shifts those numbers randomly to create some variation. We can then
+    display this array along with the curves returned from our smoothing function
+    to see how each behaves.
+    """
     t = np.linspace(-4, 4, 100)
     x = np.sin(t)
     xn = x + randn(len(t)) * 0.1
-    y = smooth(x)
+    y = graph.smooth(x)
     ws = 31
 
     # Top Graph:
@@ -40,7 +66,7 @@ def smooth_demo():
     plt.plot(x)
     plt.plot(xn)
     for w in windows:
-        plt.plot(smooth(xn,10,w))
+        plt.plot(graph.smooth(xn,10,w))
     l=['original signal', 'signal with noise']
     l.extend(windows)
 
