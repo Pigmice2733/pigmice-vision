032562a7d214c3d14915819761cf8ec823bd6c4a
Linnea Gilius <lgkuaiping@gmail.com>
2018-01-28 17:21:38 -0800

    Got nt woking with epoch and 'persistent = True'

--- networkmonitor/write.py
+++ networkmonitor/write.py
@@ -1,19 +1,29 @@
 from networktables import NetworkTables
 from networktables.util import ntproperty
 import time
+import logging
 
-
+logging.basicConfig(level=logging.DEBUG)
 NetworkTables.initialize(server='127.0.0.1')
+
+epoch = -1
+i = 0
 
 class VisionClient(object):
     '''Demonstrates an object with magic networktables properties'''
-    numcubes = ntproperty('/pigmicevsion/numcubes', 0)
+    numcubes = ntproperty('/pigmicevsion/numcubes', [epoch,i], persistent = True)
 c = VisionClient()
+time.sleep(1)
 
-
-i = 0
 while True:
-    print("numcubes",i)
-    c.numcubes = i
+    val = c.numcubes
+    print("read numcubes as", val)
+    netepoch = val[0]
+    if netepoch > epoch or netepoch == -1:
+        epoch = netepoch + 1
+    print("writing numcubes as", epoch,i)
+    c.numcubes = [epoch,i]
     time.sleep(1)
     i += 1
+
+""" assign epoch to a value in nt table"""
