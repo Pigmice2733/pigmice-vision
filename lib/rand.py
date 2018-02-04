a4682e8bfe2a0ff073fc734abaa7d262e17002e5
Howard Abrams <howard.abrams@gmail.com>
2018-02-04 15:07:55 -0800

    Add more tutorial-like comments to help with reading
    Since these functions are expected to be read as a tutorial, why not explain
    their point in the comments before the functions?

--- lib/rand.py
+++ lib/rand.py
@@ -18,6 +18,35 @@ def plot(ary, label="Frequency", marks=[]):
     plt.show()
 
 
+# First, I need some random values to play with, and it would be nice to have
+# these be /clustered/ such that if I graphed them in order, I would end up
+# with a bell curve, for instance:
+#
+#     v = np.random.normal(size=10)
+
+# Which could look like:
+#
+# [ 0.20085976  1.77831171 -0.70151459 -0.19543572  1.03648566
+#  -0.33527645  0.02794574 -0.83083124  0.18109586 -0.63390043]
+#
+# We can figure out the lowest and largest value:
+#
+#     np.max(v)   => 1.77831171288044
+#     np.min(v)   => -0.8308312386241851
+#
+# However, for my purposes, I really want everything to be a positive number,
+# so I want to /shift/ all numbers up so that the lowest number is 0. While
+# this no longer is a normal distribution, I'm fine with this for my
+# experiments.
+#
+# So we created the `shift_to_zero` function. Notice that since the minimum
+# value is /probably/ a negative value, I add its absolute value, in order to
+# /increase/ the number to at least 0, which results in transforming our array
+# like:
+#
+#     [1.031691   2.60914295 0.12931664 0.63539552 1.8673169
+#      0.49555478 0.85877698 0.         1.0119271  0.19693081]
+
 def shift_to_zero(ary):
     """
     Adds a number to every value in the given array such that the
@@ -27,6 +56,24 @@ def shift_to_zero(ary):
     increase = np.vectorize(lambda n: n + abs(lowest))
     return increase(ary)
 
+
+# I'd like to have my numbers in a range from 0 to 100, which means I need to
+# /scale/ the values by some number. Since I don't feel the need to have
+# /exactly/ 100, perhaps I could round the largest value to some integer, and
+# then get a factor dividing by 100, so we wrote `scale_to_100`.
+#
+# Suppose my largest number was 2.2, I would like to /round up/ to 3, and use
+# that value to divide by 100 in order to calculate the scaling factor, so I
+# use the =ceil= function from the =math= library.
+#
+# Calling both functions on my array gives me:
+#
+#     scale_to_100( shift_to_zero(v) )
+#
+# Returns:
+#
+#  [34.3897     86.97143172  4.3105548  21.17985059 62.24389663
+#   16.51849283 28.62589921  0.         33.73090324  6.56436021]
 
 def scale_to_100(ary):
     """
@@ -38,6 +85,11 @@ def scale_to_100(ary):
     scale = np.vectorize(lambda n: n * factor)
     return scale(ary)
 
+
+# However, in order to mimic the histogram plots we've been generating, we may
+# need /order/ them such that the largest is somewhere in the middle, and that
+# it tapers down on both ends. Something like the following function should
+# suffice:
 
 def bell_sort(ary):
     """
@@ -62,6 +114,9 @@ def bell_sort(ary):
     return np.append(f_half, np.flip(s_half, axis=-1))
 
 
+# Finally, we wrap all this in a function we can now use to investigate random
+# bell curve arrays, `random_bell`:
+
 def random_bell(size=10):
     """
     Generate a array of random numbers of the size given, but
@@ -72,6 +127,11 @@ def random_bell(size=10):
     nums = scale_to_100(shift_to_zero(v))
     return bell_sort(nums).astype(int)
 
+
+# While the `random_bell` generator will be nice under certain simplistic
+# circumstances, if we want to play around with a more histogram-like curve, we
+# can use a sophisticated function that permutes a single line into a wavy
+# curve with a maximum.
 
 def random_bumpy_peak(size=1000):
     """
