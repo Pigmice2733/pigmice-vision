4192df40b813205d888cb24d1010d07ed410f804
octoflo <flora@fuzzytoast.com>
2018-02-09 17:36:28 -0800

    Masks video based on most frequent color
    Series of files that takes an image, rewrites saved image to file,
    finds most frequent color range, and masks those colors.

--- lib/color_mask.py
+++ lib/color_mask.py
@@ -1,28 +1,29 @@
 import cv2
 import numpy as np
-from color_range import color_range
+from color_range import color_range, take_image
 import sys
 
-
-# Capture the input frame from webcam
 def get_frame(cap, scaling_factor):
-    # Capture the frame from video capture object
+    """
+    Captures the input frame from webcam
+    """
     ret, frame = cap.read()
-
     # Resize the input frame
     frame = cv2.resize(frame, None, fx=scaling_factor,
-                       fy=scaling_factor, interpolation=cv2.INTER_AREA)
-
+                        fy=scaling_factor, interpolation=cv2.INTER_AREA)
     return frame
 
 
 def run_mask():
-    cap = cv2.VideoCapture(0)
+    """
+    Only displays colors in a determined range of the most frequent colors in video feed
+    """
+    cap = cv2.VideoCapture(1)
+    # Video Capture from input 1 (external usb camera), input 0 (built in camera)
     scaling_factor = 0.5
-    lower = np.asarray([0,0,0])
-    upper = np.asarray([250,250,250])
+    lower = np.asarray([0,0,0]) # Start by showing all colors until image is inputted
+    upper = np.asarray([255,255,255])
 
-    # Iterate until the user presses ESC key
     while True:
         # Check if the user pressed ESC key
         c = cv2.waitKey(5)
@@ -30,32 +31,24 @@ def run_mask():
             break
 
         frame = get_frame(cap, scaling_factor)
-        # hsv_max = color_range.hsv_max(frame)
-        # Convert the HSV colorspace
-        hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+        hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV) # Convert the HSV colorspace
 
-        # Define 'blue' range in HSV colorspace
-        # lower = np.array([60,100,100])
-        # upper = np.array([180,255,255])
         if c == ord("c"):
+            # If "c" is pressed recall color_range function to mask again
             lower, upper = color_range(hsv)
             print("lower", lower, "upper", upper)
 
         mask = cv2.inRange(hsv, lower, upper)
+        # mask image using the range of colors given from the color_range function
 
-        # Bitwise-AND mask and original image
         res = cv2.bitwise_and(frame, frame, mask=mask)
         res = cv2.medianBlur(res, 5)
 
-        cv2.imshow('Original image', frame)
-        cv2.imshow("Mask", mask)
-        cv2.imshow("Lower Color", lower)
-        cv2.imshow("Upper Color", upper)
-        cv2.imshow('Color Detector', res)
-
-
+        cv2.imshow('Original image', frame) # Show the orginal image
+        cv2.imshow("Mask", mask) # Show the object you are masking in black and white
+        cv2.imshow('Color Detector', res) # Show image with completed mask
 
     cv2.destroyAllWindows()
 
-if __name__=='__main__':
+if __name__=='__main__': # If ran on terminal:
     run_mask()
